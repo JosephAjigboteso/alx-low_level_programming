b8a88c5bc6f3574162ff0e668828ac4491c99eb3
JosephAjigboteso <josephajigboteso@gmail.com>
2022-09-08 20:42:45 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Return 0 (Success)
+ * Return Always 0 (Success)
  */
 
 int main(void)
 {
 
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0)
+return (0);
 
-}	
+}
