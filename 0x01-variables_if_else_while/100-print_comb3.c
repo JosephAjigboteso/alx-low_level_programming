5ca23dce0c856e07cc2a9dd968d6e5958192c7ca
JosephAjigboteso <josephajigboteso@gmail.com>
2022-09-10 22:18:27 -0700

    script 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: using the main function
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+int i;
+int j;
+
+for (i = 10; i <= 19; i++)
+{
+
+for (j = 10; j <= 19; j++)
+
+{
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
+
+if (i != 19 ||  j != 1)
+{
+putchar(',');
+
+putchar(' ');
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
