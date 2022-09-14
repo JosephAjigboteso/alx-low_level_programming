a3bed2c06c5a931cda71a44fb82dab9db25e4e81
JosephAjigboteso <josephajigboteso@gmail.com>
2022-09-13 19:42:08 -0700

    islower

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -19,7 +19,26 @@ void print_alphabet(void);
  */
 void print_alphabet_x10(void);
 
+/**
+ *_islower - prints if c is lower case
+ *
+ * @c: character to use
+ *
+ * Return: 1 or 0
+ *
+ */
+int _islower(int c);
 
+/**
+ * _isalpha - prints if character is alphabet
+ *
+ * @c: character to use
+ *
+ * Return: 1 or 0
+ *
+ */
+
+int _isalpha(int c);
 
 
 
