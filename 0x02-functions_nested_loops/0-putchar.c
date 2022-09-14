31e5f71cc8100617f2da14286f50829d6e9de7e9
JosephAjigboteso <josephajigboteso@gmail.com>
2022-09-13 17:35:14 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+char _, p, u, t, c, h, a, r;
+
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ *
+ * Return: 0;
+ **/
+int _putchar(char c);
+
+
+
+
+
+
+
+
+
+
+#endif
