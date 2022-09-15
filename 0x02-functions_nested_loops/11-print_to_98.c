818f3c9b7344ad2f6ec02c015a866df43f0978b5
JosephAjigboteso <josephajigboteso@gmail.com>
2022-09-14 19:22:27 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print natural numbers from n to 98
+ * @n:parameter to start function
+ *
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+int i;
+if (n < 98)
+{
+for (i = n; i <= 98; i++)
+{
+printf("%d", i);
+if (n != 98)
+{
+printf(",");
+
+}
+
+}
+
+}
+
+if (n > 98)
+{
+for (i = n; i >= 98; i--)
+{
+printf("%d", i);
+if (n != 98)
+{
+printf(",");	}
+
+}
+}
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -93,7 +93,12 @@ void times_table(void);
  */
 int add(int, int);
 
-
+/**
+ * print_t_98 - natural numbers from n to 98
+ *
+ *@n - integer to put in this table
+ */
+ void print_to_98(int n);
 
 
 
