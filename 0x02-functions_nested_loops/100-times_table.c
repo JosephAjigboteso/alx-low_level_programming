d5878ce1fb80b966b8e5b9b7c0ca5a4cde3f7a29
JosephAjigboteso <josephajigboteso@gmail.com>
2022-09-14 20:42:31 -0700

    The World looks like a multiplication-table,

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,33 +18,26 @@ for (j = 0; j <= n; j++)
 {
 int prod = j * i;
 if (j == 0)
-{
-_putchar('0');
-}
+{ _putchar('0'); }
 else if (prod <= 9)
-{
-_putchar(',');
+{ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(' ');
-_putchar('0' + prod);
-}
+_putchar('0' + prod); }
 else if (prod > 99)
-{
-_putchar(',');
+{ _putchar(',');
 _putchar(' ');
 _putchar('0' + (prod / 100));
 _putchar('0' + ((prod / 10) % 10));
 _putchar('0' + (prod % 10));
 }
 else
-{
-_putchar(',');
+{ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar('0' + (prod / 10));
-_putchar('0' + (prod % 10));
-}
+_putchar('0' + (prod % 10)); }
 }
 _putchar('\n');
 }
