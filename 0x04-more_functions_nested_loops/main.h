5598cb4eca3b75bb6e3f33d8f192e8c433a6da0f
JosephAjigboteso <josephajigboteso@gmail.com>
2022-09-15 16:09:19 -0700

    I feel like I am diagonally parked in a parallel universe

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -58,6 +58,17 @@ void more_numbers(void);
  */
 void print_line(int n);
 
+/**
+ * print_diagonal - print lines diagonally
+ * @n: papramter to determine number of lines
+ */
+void print_diagonal(int n);
+
+
+
+
+
+
 
 
 #endif
