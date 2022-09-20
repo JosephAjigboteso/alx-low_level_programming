62adceb45fd0bc28cccf0c5c5ed9cb508f30f661
JosephAjigboteso <josephajigboteso@gmail.com>
2022-09-19 21:49:31 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * int_strlem - returns length of string
+ * @s: character to get string
+ * Return: returns length
+ */
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{}
+return (i);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -24,5 +24,9 @@ void reset_to_98(int *n);
  */
 void swap_int(int *a, int *b);
 
+/**
+ * int_strlen - returns length of string
+ */
+int _strlen(char *s);
 
 #endif
