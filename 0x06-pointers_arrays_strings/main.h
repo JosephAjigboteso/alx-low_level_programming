2119e722bbe8a0232e22d94b655711c1d5b2439c
JosephAjigboteso <josephajigboteso@gmail.com>
2022-09-22 15:14:21 -0700

    strncat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,12 +1,10 @@
 #ifndef _MAIN_
 #define _MAIN_
-#include <stdio.h>
 /**
  * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- *  @c: The character to print
- *
- *  Return: On success 1.
+ * Return: On success 1.
  *
  * On error, -1 is returned, and errno is set appropriately.
  *
@@ -14,15 +12,21 @@
 int _putchar(char c);
 
 /**
- * _strcat - append string src to string dest overwriting null character
- * @dest: first string to be overwritten
- * @src: second string
- * Return: pointer to string dest
+ * _strcat - cocatenate two strings
+ * @src: first
+ * @dest: second string
+ * Return: dest
  */
 char *_strcat(char *dest, char *src);
 
-
-
+/**
+ * _strncat - cat strings
+ * @dest: 1
+ * @src:2
+ * @n:n
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n);
 
 
 
