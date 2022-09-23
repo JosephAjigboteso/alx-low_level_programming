ffd0a69d2fbfe03c7dcfd9917ab6aa1aeb4ef850
JosephAjigboteso <josephajigboteso@gmail.com>
2022-09-23 06:20:06 -0700

    Write a function that adds two numbers.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -90,4 +90,27 @@ char *rot13(char *);
  */
 void print_number(int n);
 
+
+/**
+ * infinite_add - adds two numbers
+ *
+ * @n1: first number
+ *
+ * @n2: second number
+ *
+ * @r: buffer for result
+ *
+ * @size_r: buffer size
+ *
+ * Return: address of r or 0
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+
+
+
+
+
+
+
 #endif
