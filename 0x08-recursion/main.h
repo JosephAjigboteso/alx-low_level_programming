78c8298f0dc45279bec27eeb62742a0c1c113f9c
JosephAjigboteso <josephajigboteso@gmail.com>
2022-09-28 14:28:26 -0700

    task 0

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,22 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+ * _putchar - writes the character c to stdout
+ *
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ *
+ * On error, -1 is returned, and errno is set appropriately.
+ *
+ */
+int _putchar(char c);
+
+/**
+ * _puts_recursion - print out string using recursion
+ * @s: string to be printed
+ */
+void _puts_recursion(char *s);
+
+#endif
