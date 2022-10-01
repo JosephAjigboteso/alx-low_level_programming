9f78b15198c155abe27655791cd1cf0bfd1ed2e0
JosephAjigboteso <josephajigboteso@gmail.com>
2022-09-30 19:28:05 -0700

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-/**                                                                                                          * main - print command executuon strings                                                                    
- * @argc: detemines number of argumets passed when executing file                       
- * @argv: prints the strings                                                                                 
- * Return: 0                                                                                                 */ 
+/**
+ * main - print command executuon strings
+ * @argc: detemines number of argumets passed when executing file
+ * @argv: prints the strings
+ * Return: 0
+ */
 int main(int argc, __attribute__((unused))char *argv[])
 {
 printf("%d\n", argc - 1);
