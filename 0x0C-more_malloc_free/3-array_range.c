254a27aca43ac8235e10a53c9afe9dc1ee29afa3
JosephAjigboteso <josephajigboteso@gmail.com>
2022-10-07 18:39:55 -0700

    Task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - create an array of strings
+ * @min: starting point 
+ * @max: end string point
+ * Return: pointer t the array
+ */
+
+int *array_range(int min, int max)
+{
+int *ptr;
+int i, len;
+if (min > max)
+{
+return (NULL);
+}
+for (i = min; i <= max; i++)
+{
+len++;
+}
+ptr = malloc(sizeof(int) * len);
+if (ptr == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < len; i++)
+{
+ptr[i] = min;
+min++;
+}
+return (ptr);
+}
