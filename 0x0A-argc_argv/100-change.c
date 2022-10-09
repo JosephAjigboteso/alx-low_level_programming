ba20927515e6bb6bd3a0ccb85d2971ee4af88185
JosephAjigboteso <josephajigboteso@gmail.com>
2022-10-08 17:52:34 -0700

    Task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -20,25 +20,25 @@ return (1);
 money = atoi(argv[argc - 1]);
 while (money > 0)
 {
-if (money % 25 == 0)
+if (money >= 25)
 {
 money -= 25;
 }
-if (money % 10 == 0)
+else if (money >= 10)
 {
 money -= 10;
 }
-if (money % 5 == 0)
+else if (money >= 5)
 {
 money -= 5;
 }
-if (money % 2 == 0)
+else if (money >= 2)
 {
 money -= 2;
 }
-else
+else if (money >= 1)
 {
-money--;
+money -= 1;
 }
 coins++;
 }
