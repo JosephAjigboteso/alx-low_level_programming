ad80d5f2ee9d51f40656f165242df1ad75b76eec
JosephAjigboteso <josephajigboteso@gmail.com>
2022-10-12 17:00:44 -0700

    fix betty task 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
 #include "function_pointers.h"
 #include <stddef.h>
 /**
- * prints name - This function prints name by calling other functions
+ * print_name - This function prints name by calling other functions
  * @name: name to be printed
  * @f: pointer to functions
  */
 void print_name(char *name, void (*f)(char *))
 {
 if (f != NULL && name != NULL)
-{	
+{
 f(name);
 }
 }
