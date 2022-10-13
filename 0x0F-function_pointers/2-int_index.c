4f283f9411b358bdf7af588668082e7c16ba73f4
JosephAjigboteso <josephajigboteso@gmail.com>
2022-10-12 18:12:49 -0700

    tASK 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * int_index - seraches for an integer by calling other functions
+ * @array: array of numbers
+ * @size: number of elements in the array
+ * @cmp: pointer to functions
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (size <= 0 || array == NULL || cmp == NULL)
+{
+return (-1);
+}
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+{
+return (i);
+}
+}
+return (-1);
+}
