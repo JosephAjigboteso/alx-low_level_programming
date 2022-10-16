7fb2a992071b62759923ff2eaa1270911042222a
JosephAjigboteso <josephajigboteso@gmail.com>
2022-10-16 10:53:24 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,7 @@
 #include "main.h"
+#include <string.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * str_concat - concatenate two strings
  * @s1: first string
@@ -11,21 +12,9 @@ char *str_concat(char *s1, char *s2)
 {
 int a, b, i;
 char *my_array;
-a = 0;
-b = 0;
 i = 0;
-while (s1[i] != '\0')
-{
-i++;
-a++;
-}
-
-while (s2[i] != '\0')
-{
-i++;
-b++;
-}
-
+a = strlen(s1);
+b = strlen(s2);
 my_array = malloc(sizeof(char) * (a + b + 1));
 if (my_array == NULL)
 {
@@ -44,11 +33,10 @@ while (i < a)
 my_array[i] = s1[i];
 i++;
 }
-while (i < b)
+while (i <= b)
 {
 my_array[i + a] = s2[i];
 i++;
 }
-my_array[i + a] = '\0';
 return (my_array);
 }
