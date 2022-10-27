dbd5662d6b53380765bd820425fcd1d3345d29b2
JosephAjigboteso <josephajigboteso@gmail.com>
2022-10-26 17:27:46 -0700

    task 1 more  singlelinked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
  * listint_len - returns number of elements in a linked list
+ * @h: equal to the head first address of list
  * Return: number of elemets in the list
  */
 size_t listint_len(const listint_t *h)
